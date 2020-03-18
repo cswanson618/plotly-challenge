c82e77f037b749a7c5e698df7e639e94a99e0e8e
cswanson618 <54300477+cswanson618@users.noreply.github.com>
2020-03-17 21:44:04 -0500

    Ran prettier on app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,14 @@
 function buildMetadata(sample) {
   d3.json("/metadata/" + sample).then(jsons => {
-    const panel = d3.select("#sample-metadata")
-    panel.html("")
-    Object.entries(jsons).forEach(([key, value]) => panel.append("p").html(`${key}:${value}`))
-  })}
-    
+    const panel = d3.select("#sample-metadata");
+    panel.html("");
+    Object.entries(jsons).forEach(([key, value]) =>
+      panel.append("p").html(`${key}:${value}`)
+    );
+  });
+}
+
 //credit to MDN, Daniela Matos (especially!!!) and StackOverflow for help
-    
 
 function buildCharts(sample) {
   d3.json("/samples/" + sample).then(plotdata => {
@@ -14,53 +16,53 @@ function buildCharts(sample) {
       x: plotdata.otu_ids,
       y: plotdata.sample_values,
       text: plotdata.otu_labels,
-      mode: 'markers',
+      mode: "markers",
       marker: {
         color: plotdata.otu_ids,
         size: plotdata.sample_values
       }
     };
-    
+
     var data = [trace1];
-    
+
     var layout = {
-      title: 'Samples Bubble Chart (each sample generates its own chart!)',
+      title: "Samples Bubble Chart (each sample generates its own chart!)",
       showlegend: false,
       height: 600,
       width: 1200
     };
-    
-    Plotly.newPlot('bubble', data, layout);
 
-    var data = [{
-      values: plotdata.sample_values.slice(0, 10),
-      labels: plotdata.otu_ids.slice(0, 10),
-      hovertext: plotdata.otu_labels.slice(0, 10),
-      hoverinfo: 'hovertext',
-      title: 'Samples Pie Chart (each sample generates its own chart!)',
-      type: 'pie'
-    }];
-    
+    Plotly.newPlot("bubble", data, layout);
+
+    var data = [
+      {
+        values: plotdata.sample_values.slice(0, 10),
+        labels: plotdata.otu_ids.slice(0, 10),
+        hovertext: plotdata.otu_labels.slice(0, 10),
+        hoverinfo: "hovertext",
+        title: "Samples Pie Chart (each sample generates its own chart!)",
+        type: "pie"
+      }
+    ];
+
     var layout = {
       height: 400,
       width: 500
     };
-    
-    Plotly.newPlot('pie', data, layout);
-  })}
 
-    // Hovertext aid from Plotly Documentation
+    Plotly.newPlot("pie", data, layout);
+  });
+}
 
-    
-
+// Hovertext aid from Plotly Documentation
 
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
+  d3.json("/names").then(sampleNames => {
+    sampleNames.forEach(sample => {
       selector
         .append("option")
         .text(sample)
